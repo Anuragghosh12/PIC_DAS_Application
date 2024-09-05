62efedbc4f34b3c806ee5074e56c22a44a989deb
Anurag Ghosh <115475401+Anuragghosh12@users.noreply.github.com>
2024-09-05 14:16:15 +0530

    Bug Fixes for main

--- SerialReaderWriter/auto_port_detection.py
+++ SerialReaderWriter/auto_port_detection.py
@@ -2,21 +2,33 @@ import serial
 import serial.tools.list_ports
 
 def find_device_port(baudrate=38400, timeout=1):
+    serial_ports = []
     available_ports = serial.tools.list_ports.comports()
     for port in available_ports:
         try:
-            ser = serial.Serial(port.device, baudrate=baudrate, timeout=timeout)
-            ser.close()  # Immediately close the port after a successful open
-            print(f"Device found on port: {port.device}")
-            return port.device
-        except (OSError, serial.SerialException):
-            pass
-    return None
+            port_name = port.device
+            with serial.Serial(port.device, baudrate=baudrate, timeout=timeout) as ser:
+                ser.close()  # Immediately close the port after a successful open
+            serial_ports.append(port_name)
+            print(f"Device found on port: {port_name}")
 
-# Usage example
-if __name__ == "__main__":
-    device_port = find_device_port()
-    if device_port:
-        print(f"Auto-detected port: {device_port}")
+        except serial.SerialException as e:
+            print(f"Could not open port{port_name}: {e}")
+        except PermissionError :
+            print(f"Permission denied for port{port_name}")
+
+    if serial_ports:
+        print(f"Available ports : {serial_ports}")
+        return serial_ports
     else:
-        print("No device found on any port.")
+        print(f"No available ports found")
+        return None
+
+
+# # Usage example
+# if __name__ == "__main__":
+#     device_port = find_device_port()
+#     if device_port:
+#         print(f"Auto-detected port: {device_port}")
+#     else:
+#         print("No device found on any port.")
